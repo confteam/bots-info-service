1e23b2e257c32acec1315213d87f89d21985772f
ensomnatt <ensomnatt@protonmail.com>
2025-09-11 20:53:18 +0300

    archive

--- internal/handlers/register.go
+++ internal/handlers/register.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/confteam/bots-info-service/internal/dto"
 	"github.com/confteam/bots-info-service/internal/logger"
+	"github.com/confteam/bots-info-service/internal/repository"
+	"github.com/jackc/pgx/v5"
 )
 
 func (h Handler) Register(w http.ResponseWriter, r *http.Request) {
@@ -20,12 +22,23 @@ func (h Handler) Register(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := h.CreateContext()
 	defer cancel()
 
-	bot, err := h.repo.RegisterBot(ctx, req)
+	var result *repository.Bot
+
+	bot, err := h.repo.FindBot(ctx, req)
 	if err != nil {
-		http.Error(w, "Failed to register bot "+err.Error(), http.StatusInternalServerError)
-		return
+		if err == pgx.ErrNoRows {
+			result, err = h.repo.RegisterBot(ctx, req)
+			if err != nil {
+				http.Error(w, "Failed to register bot: "+err.Error(), http.StatusBadRequest)
+				return
+			}
+		} else {
+			http.Error(w, "Failed to find bot: "+err.Error(), http.StatusNotFound)
+		}
+	} else {
+		result = bot
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(bot)
+	json.NewEncoder(w).Encode(result)
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -11,13 +11,13 @@ import (
 )
 
 type Bot struct {
-	ID         int    `json:"id"`
-	Token      string `json:"token"`
-	Confession string `json:"confession"`
-	ChatID     string `json:"chatId"`
-	ChannelID  string `json:"channelId"`
-	Type       string `json:"type"`
-	Code       string `json:"code"`
+	ID         int     `json:"id"`
+	Token      string  `json:"token"`
+	Confession *string `json:"confession"`
+	ChatID     *string `json:"chatId"`
+	ChannelID  *string `json:"channelId"`
+	Type       *string `json:"type"`
+	Code       *string `json:"code"`
 }
 
 type BotRepository struct {
@@ -36,8 +36,8 @@ func (r *BotRepository) RegisterBot(ctx context.Context, dto dto.RegisterDto) (*
 
 	bot := &Bot{
 		Token: dto.Token,
-		Type:  dto.Type,
-		Code:  code,
+		Type:  &dto.Type,
+		Code:  &code,
 	}
 
 	query := "INSERT INTO bots (token, type, code) VALUES ($1, $2, $3) RETURNING id"
@@ -49,4 +49,18 @@ func (r *BotRepository) RegisterBot(ctx context.Context, dto dto.RegisterDto) (*
 	logger.Log.Info("registered bot", "type", dto.Type)
 
 	return bot, nil
+}
+
+func (r *BotRepository) FindBot(ctx context.Context, dto dto.RegisterDto) (*Bot, error) {
+	var bot Bot
+
+	query := "SELECT id, token, confession, chat_id, channel_id, type, code FROM bots WHERE token=$1"
+	err := r.db.QueryRow(ctx, query, dto.Token).Scan(&bot.ID, &bot.Token, &bot.Confession, &bot.ChatID, &bot.ChannelID, &bot.Type, &bot.Code)
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Log.Info("selected bot", "type", dto.Type)
+
+	return &bot, nil
 }
